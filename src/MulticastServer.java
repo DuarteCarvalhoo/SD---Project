f45958aeaa80858dfc0d08f1a328b153eadafc67
Duarte Carvalho <duarte38carvalho@gmail.com>
2018-10-25 21:30:16 +0100

    so quero ver os updates

--- src/MulticastServer.java
+++ src/MulticastServer.java
@@ -44,115 +44,111 @@ public class MulticastServer extends Thread implements Serializable {
                 System.out.println(msg);
                 //try { sleep((long) (Math.random() * SLEEP_TIME)); } catch (InterruptedException e) { }
                 String[] aux = msg.split(";");
-                if (!(aux[0].equals("type|login")) && !(aux[0].equals("type|register")) && !(aux[0].equals("type|logout"))) {
-                    System.out.println("Rejected");
-                } else {
-                    switch (aux[0]) {
-                        case "type|login":
-                            boolean flag = false;
-                            String[] loginUsernameParts = aux[1].split("\\|");
-                            String[] loginPasswordParts = aux[2].split("\\|");
-                            String user = loginUsernameParts[1];
-                            String pass = loginPasswordParts[1];
-                            System.out.println("USERNAME: " + user + " PASSWORD: " + pass);
-                            if (usersList.isEmpty()) {
-                                sendMsg("type|loginFail");
-                                System.out.println("ERRO: No users on the database.");
-                            } else {
-                                for (User u : usersList) {
-                                    if (user.equals(u.getUsername())) {
-                                        if (u.isOnline()) {
+                switch (aux[0]) {
+                    case "type|login":
+                        boolean flag = false;
+                        String[] loginUsernameParts = aux[1].split("\\|");
+                        String[] loginPasswordParts = aux[2].split("\\|");
+                        String user = loginUsernameParts[1];
+                        String pass = loginPasswordParts[1];
+                        System.out.println("USERNAME: " + user + " PASSWORD: " + pass);
+                        if (usersList.isEmpty()) {
+                            sendMsg("type|loginFail");
+                            System.out.println("ERRO: No users on the database.");
+                        } else {
+                            for (User u : usersList) {
+                                if (user.equals(u.getUsername())) {
+                                    if (u.isOnline()) {
+                                        sendMsg("type|loginFail");
+                                        System.out.println("ERRO: User already logged in.");
+                                    } else {
+                                        boolean loggedInSuccessfully = checkUsernameLogin(user, pass);
+                                        if (!loggedInSuccessfully) {
                                             sendMsg("type|loginFail");
-                                            System.out.println("ERRO: User already logged in.");
+                                            System.out.println("ERRO: Login não completo.");
                                         } else {
-                                            boolean loggedInSuccessfully = checkUsernameLogin(user, pass);
-                                            if (!loggedInSuccessfully) {
-                                                sendMsg("type|loginFail");
-                                                System.out.println("ERRO: Login não completo.");
-                                            } else {
-                                                u.setOnline();
-                                                sendMsg("type|loginComplete;username|" + u.getUsername() + ";password|" + u.getPassword() + ";editor|" + u.isEditor() + ";online|" + u.isOnline());
-                                                System.out.println("SUCESSO: Login Completo");
-                                                flag = true;
-                                            }
+                                            u.setOnline();
+                                            sendMsg("type|loginComplete;username|" + u.getUsername() + ";password|" + u.getPassword() + ";editor|" + u.isEditor() + ";online|" + u.isOnline());
+                                            System.out.println("SUCESSO: Login Completo");
+                                            flag = true;
                                         }
                                     }
                                 }
-                                if (!flag) {
-                                    System.out.println("Username not found.");
-                                    sendMsg("type|loginFail");
+                            }
+                            if (!flag) {
+                                System.out.println("Username not found.");
+                                sendMsg("type|loginFail");
+                            }
+                        }
+                        //funçao passa como argumentos o user e pw
+                        //funçao pra confirmar se o user existe, se a pw ta certa e por fim enviar a resposta
+                        break;
+                    case "type|register":
+                        aux2 = aux[1];
+                        String[] registerUsernameParts = aux2.split("\\|");
+                        String[] registerPasswordParts = aux[2].split("\\|");
+                        String username = registerUsernameParts[1];
+                        String password = registerPasswordParts[1];
+                        System.out.println("USERNAME: " + username + " PASSWORD: " + password);
+                        int usernameUsed = checkUsernameRegister(username);
+                        if (usernameUsed == 1) {
+                            sendMsg("type|usernameUsed");
+                            System.out.println("ERRO: Username já usado.");
+                        } else if (usernameUsed == -1) {
+                            User newUser = new User(username, password);
+                            newUser.makeEditor();
+                            System.out.println("Editor permisions given.");
+                            usersList = writeFilesUser(usersList, newUser);
+                            System.out.println("SUCCESS: User added to database with username: '" + username + "' and password '" + password + "'");
+                            sendMsg("type|registComplete");
+                        } else {
+                            User newUser = new User(username, password);
+                            usersList = writeFilesUser(usersList, newUser);
+                            System.out.println("SUCESSO: Adicionou ao arraylist com user '" + username + "' e password '" + password + "'");
+                            sendMsg("type|registComplete");
+                        }
+                        //funçao passa como argumentos o user e pw
+                        //na funçao verificar se nao ha users iguais, se nao guardar no arraylist (se usarmos 2 pws ver se sao iguais) e enviar a resposta
+                        break;
+                    case "type|turnOnSocket":
+                        aux2 = aux[1];
+                        String[] address = aux2.split("\\|");
+                        socketHelp = ligarSocket(address[1]);
+                        //String[] musicName = aux[2].split("\\|");
+                        //receiveMusic(address[1], musicName[1]);
+                        sendMsg("Music saving on the server.");
+                        break;
+                    case "type|sendMusic":
+                        aux2 = aux[1];
+                        String[] musicName = aux2.split("\\|");
+                        receiveMusic(socketHelp, musicName[1]);
+                        sendMsg("it worked out");
+                    case "type|logout":
+                        boolean flag1 = false;
+                        System.out.println("tou a dar logout");
+                        String[] logoutUsername = aux[1].split("\\|");
+                        String logoutUser = logoutUsername[1];
+                        if (!logoutUser.equals("none")) {
+                            for (User u : usersList) {
+                                if (u.getUsername().equals(logoutUser)) {
+                                    u.setOffline();
+                                    sendMsg("type|logoutComplete");
+                                    flag1 = true;
                                 }
                             }
-                            //funçao passa como argumentos o user e pw
-                            //funçao pra confirmar se o user existe, se a pw ta certa e por fim enviar a resposta
-                            break;
-                        case "type|register":
-                            aux2 = aux[1];
-                            String[] registerUsernameParts = aux2.split("\\|");
-                            String[] registerPasswordParts = aux[2].split("\\|");
-                            String username = registerUsernameParts[1];
-                            String password = registerPasswordParts[1];
-                            System.out.println("USERNAME: " + username + " PASSWORD: " + password);
-                            int usernameUsed = checkUsernameRegister(username);
-                            if (usernameUsed == 1) {
-                                sendMsg("type|usernameUsed");
-                                System.out.println("ERRO: Username já usado.");
-                            } else if (usernameUsed == -1) {
-                                User newUser = new User(username, password);
-                                newUser.makeEditor();
-                                System.out.println("Editor permisions given.");
-                                usersList = writeFilesUser(usersList, newUser);
-                                System.out.println("SUCCESS: User added to database with username: '" + username + "' and password '" + password + "'");
-                                sendMsg("type|registComplete");
-                            } else {
-                                User newUser = new User(username, password);
-                                usersList = writeFilesUser(usersList, newUser);
-                                System.out.println("SUCESSO: Adicionou ao arraylist com user '" + username + "' e password '" + password + "'");
-                                sendMsg("type|registComplete");
-                            }
-                            //funçao passa como argumentos o user e pw
-                            //na funçao verificar se nao ha users iguais, se nao guardar no arraylist (se usarmos 2 pws ver se sao iguais) e enviar a resposta
-                            break;
-                        case "type|turnOnSocket":
-                            aux2 = aux[1];
-                            String[] address = aux2.split("\\|");
-                            socketHelp = ligarSocket(address[1]);
-                            //String[] musicName = aux[2].split("\\|");
-                            //receiveMusic(address[1], musicName[1]);
-                            sendMsg("Music saving on the server.");
-                            break;
-                        case "type|sendMusic":
-                            aux2 = aux[1];
-                            String[] musicName = aux2.split("\\|");
-                            receiveMusic(socketHelp, musicName[1]);
-                            sendMsg("it worked out");
-                        case "type|logout":
-                            boolean flag1 = false;
-                            System.out.println("tou a dar logout");
-                            String[] logoutUsername = aux[1].split("\\|");
-                            String logoutUser = logoutUsername[1];
-                            if (!logoutUser.equals("none")) {
-                                for (User u : usersList) {
-                                    if (u.getUsername().equals(logoutUser)) {
-                                        u.setOffline();
-                                        sendMsg("type|logoutComplete");
-                                        flag1 = true;
-                                    }
-                                }
-                                if (!flag1) {
-                                    System.out.println("erro nos ficheiros");
-                                    sendMsg("type|logoutFail");
-                                }
-                            } else {
-                                System.out.println("No user is logged in.");
+                            if (!flag1) {
+                                System.out.println("erro nos ficheiros");
                                 sendMsg("type|logoutFail");
                             }
-                            break;
-                        //dentro da funçao decidir o que pesquisa artista, estilo ou album
-                        default:
-                            System.out.println("Default");
-                            break;
-                    }
+                        } else {
+                            System.out.println("No user is logged in.");
+                            sendMsg("type|logoutFail");
+                        }
+                        break;
+                    //dentro da funçao decidir o que pesquisa artista, estilo ou album
+                    default:
+                        System.out.println("Default");
+                        break;
                 }
             }
         } catch (IOException e) {
